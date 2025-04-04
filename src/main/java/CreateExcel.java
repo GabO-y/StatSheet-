91482a0163aa0e3d132cd82102b917b3b633cdac
GabO-y <gabrielosilva1000@gmail.com>
2025-04-04 19:25:13 -0300

    Media moda mediana variancia desvio implementado

--- src/main/java/CreateExcel.java
+++ src/main/java/CreateExcel.java
@@ -1,73 +1,39 @@
+import org.apache.commons.lang3.mutable.Mutable;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.xmlbeans.impl.xb.xsdschema.AnyDocument;
 
 import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.text.NumberFormat;
 import java.util.*;
 
 public class CreateExcel {
 
     static Workbook w = new XSSFWorkbook();
     static Sheet sheet = w.createSheet("Planilha");
+    FormulaEvaluator evaluator = w.getCreationHelper().createFormulaEvaluator();
+    CellValue valueCell;
+    List<Double> values;
+    String intervalValues;
+    List<Row> rows = new ArrayList<>();
+    Map<String, Double> infos;
 
-    static FormulaEvaluator evaluator = w.getCreationHelper().createFormulaEvaluator();
-    static CellValue value;
-    static List<Row> rows = new ArrayList<>();
-    static Map<String, Double> infos;
+    CellStyle style = w.createCellStyle();
+    DataFormat format = w.createDataFormat();
 
-    public static void main(String[] args) {
+    public CreateExcel(List<Double> values) {
 
-        for (int i = 0; i <= 6; i++) {
-            rows.add(sheet.createRow(i));
-        }
+        this.values = values;
 
+        Collections.sort(values);
 
+        setListValues(this.values);
 
         titles();
 
-        //var list = List.of(10.0, 34.9, 80.8, 22.4, 10.0, 34.9, 80.8, 22.4, 10.0, 34.9, 80.8, 22.4, 10.0, 34.9, 80.8, 22.4);
+        infos = infos(values);
 
-        List<Double> list = new ArrayList<>();
+        classes();
 
-
-        var r = new Random();
-
-        for (int i = 0; i < 100; i++) {
-            list.add(r.nextDouble(1.0, 10000000.0));
-        }
-
-        infos = infos(list);
-        Collections.sort(list);
-
-        CellStyle style = w.createCellStyle();
-        DataFormat format = w.createDataFormat();
-        style.setDataFormat(format.getFormat("0.00"));
-
-        for (int i = 0, l = 1; i < list.size(); i++, l++) {
-
-            if (l > 6) {
-
-                rows.add(sheet.createRow(l));
-
-            }
-            rows.get(l).createCell(0).setCellValue(list.get(i));
-            rows.get(l).getCell(0).setCellStyle(style);
-
-        }
-
-        //Intervalo dos valores
-        String valoresIntevals = "A2:A" + (list.size() + 1);
-
-
-        String c = "$D$7";
-
-        int i = addClass(2);
-
-        cell("D6").setCellValue(i);
-
-        frequency(valoresIntevals);
+        frequency(intervalValues);
 
         setMidPoint();
 
@@ -93,10 +59,62 @@ public class CreateExcel {
             System.out.println("Erro ao criar arquivo\n" + e.getMessage());
         }
 
-
     }
 
-    public static Map<String, Double> infos(List<Double> list) {
+
+    public static void main(String[] args) {
+
+        List<Double> values = new ArrayList<>();
+
+        values = List.of(
+        19351.0,
+        19352.0,
+        19350.0,
+        19353.0,
+        19354.0,
+        19355.0,
+        19356.0,
+        19357.0,
+        19358.0,
+        19359.0,
+        19360.0,
+        19361.0,
+        19362.0,
+        19363.0,
+        19364.0,
+        19365.0,
+        19366.0,
+        19367.0,
+        19368.0,
+        19369.0,
+        19370.0,
+        19371.0,
+        19372.0,
+        19373.0,
+        19374.0,
+        19375.0,
+        19376.0,
+        19377.0,
+        19378.0,
+        19379.0,
+        19380.0,
+        19381.0,
+        19382.0,
+        19383.0,
+        19384.0
+);
+
+        values = new ArrayList<>(values);
+
+//        var r = new Random();
+//
+//        for(int i = 0; i < 10000; i++){
+//            values.add(r.nextDouble(1.0, 100000.0));
+//        }
+
+        CreateExcel tb = new CreateExcel(values);
+    }
+    public Map<String, Double> infos(List<Double> list) {
 
         Map<String, Double> infos = new HashMap<>();
 
@@ -104,6 +122,7 @@ public class CreateExcel {
         double max = Double.MIN_VALUE;
 
         for (double i : list) {
+
             if (i <= min) {
                 min = i;
             }
@@ -158,13 +177,27 @@ public class CreateExcel {
 
     }
 
-    public static void titles(){
+    public void setListValues(List<Double> list) {
+
+        this.values = list;
+
+        style.setDataFormat(format.getFormat("0.00"));
+
+        for (int i = 2, l = 0; i < list.size() + 2; i++, l++) {
+            cell("A" + i).setCellValue(values.get(l));
+            cell("A" + i).setCellStyle(style);
+        }
+
+        this.intervalValues = "A2:A" + (values.size() + 1);
+    }
+
+    public void titles() {
 
         cell("A1").setCellValue("Valores");
 
-        String[] information = new String[] {"N", "Xmax", "Xmin", "At","K", "C"};
+        String[] information = new String[]{"N", "Xmax", "Xmin", "At", "K", "C"};
 
-        for(int i = 2, l = 0; i <= 7; i++,l++){
+        for (int i = 2, l = 0; i <= 7; i++, l++) {
             cell("B" + i).setCellValue(information[l]);
         }
 
@@ -179,60 +212,82 @@ public class CreateExcel {
         cell("M2").setCellValue("F↑");
         cell("N2").setCellValue("F↑%");
 
-        information = new String[] {
-                "COUNT(" + "A2:A101" + ")",
-                "MAX(" + "A2:A101" + ")",
-                "MIN(" + "A2:A101" + ")",
+        information = new String[]{
+                "COUNT(" + intervalValues + ")",
+                "MAX(" + intervalValues + ")",
+                "MIN(" + intervalValues + ")",
                 "C3-C4",
                 "1 + 3.3 * LOG10(C2)",
                 "C5/C6"
         };
 
-        for(int i = 2, l = 0; i <= 7; i++, l++){
+        for (int i = 2, l = 0; i <= 7; i++, l++) {
             cell("C" + i).setCellFormula(information[l]);
         }
 
         cell("D6").setCellFormula("ROUND(C6, 0)");
-        cell("D7").setCellFormula("CEILING(C7, 5)");
+        cell("D7").setCellFormula("CEILING(C7, 1)");
 
+        information = new String[] {"MEDIA", "MODA", "MEDIANA", "VARIÂNCIA", "DESVIO PADRÃO"};
+
+        for(int i = 10, l = 0; i <= 14; i++, l++){
+            cell("B" + i).setCellValue(information[l]);
+        }
+
+        information = new String[]{
+                "AVERAGE(" + intervalValues + ")",
+                "MODE(" + intervalValues + ")",
+                "MEDIAN(" + intervalValues + ")",
+                "VAR(" + intervalValues + ")",
+                "STDEV(" + intervalValues + ")"
+        };
+
+        for(int i = 10, l = 0; i <= 14; i++, l++){
+            cell("C" + i).setCellFormula(information[l]);
+        }
+
+        style.setDataFormat(format.getFormat("0.000"));
+
+        for(int i = 3; i <= 14; i++){
+            cell("C" + i).setCellStyle(style);
+        }
 
     }
-
-    public static int addClass(int i2) {
+    public int addClass(int i2) {
 
         //Adiciona as duas primeiras linhas necessarias para construir as classes
-        sheet.getRow(2).createCell(4).setCellFormula(infos.get("minL").toString());
-        sheet.getRow(2).createCell(5).setCellFormula("E3+$D$7");
+        cell("E3").setCellFormula(infos.get("minL").toString());
+        cell("F3").setCellFormula("E3+$D$7");
 
         evaluator = w.getCreationHelper().createFormulaEvaluator();
-        value = evaluator.evaluate(sheet.getRow(i2).getCell(5));
+        valueCell = evaluator.evaluate(cell("F" + i2));
 
-        var x = value.getNumberValue();
+        var x = valueCell.getNumberValue();
 
         i2++;
 
         while (x < infos.get("Xmax")) {
 
-            if (sheet.getRow(i2) == null) {
-                rows.add(sheet.createRow(i2));
-                rows.get(i2).createCell(4);
-                rows.get(i2).createCell(5);
-            }
-            if (rows.get(i2).getCell(4) == null) {
-                rows.get(i2).createCell(4);
-            }
-            if (rows.get(i2).getCell(5) == null) {
-                rows.get(i2).createCell(5);
-            }
+//            if (sheet.getRow(i2) == null) {
+//                rows.add(sheet.createRow(i2));
+//                rows.get(i2).createCell(4);
+//                rows.get(i2).createCell(5);
+//            }
+//            if (rows.get(i2).getCell(4) == null) {
+//                rows.get(i2).createCell(4);
+//            }
+//            if (rows.get(i2).getCell(5) == null) {
+//                rows.get(i2).createCell(5);
+//            }
 
-            sheet.getRow(i2).getCell(4).setCellFormula("F" + i2);
-            sheet.getRow(i2).getCell(5).setCellFormula("E" + (i2 + 1) + " + " + "$D$7");
+            cell("E" + i2).setCellFormula("F" + i2);
+            cell("F" + i2).setCellFormula("E" + (i2 + 1) + " + " + "$D$7");
 
             evaluator = w.getCreationHelper().createFormulaEvaluator();
-            value = evaluator.evaluate(sheet.getRow(i2).getCell(5));
+            valueCell = evaluator.evaluate(cell("F" + i2));
 
             //Apenas para receber o valor da celula
-            x = value.getNumberValue();
+            x = valueCell.getNumberValue();
 
             i2++;
 
@@ -253,17 +308,17 @@ public class CreateExcel {
 
             //Pega celula do limite inferior
 
-            value = evaluator.evaluate(cell("E" + i));
+            valueCell = evaluator.evaluate(cell("E" + i));
 
-            if (value == null) {
+            if (valueCell == null) {
                 break;
             }
 
-            var li = value.getNumberValue();
+            var li = valueCell.getNumberValue();
 
-            value = evaluator.evaluate(cell("F" + i));
+            valueCell = evaluator.evaluate(cell("F" + i));
 
-            var ls = value.getNumberValue();
+            var ls = valueCell.getNumberValue();
 
             NumberFormat frt = NumberFormat.getInstance(Locale.getDefault());
 
@@ -282,16 +337,60 @@ public class CreateExcel {
 
     }
     */
-    public static void frequency(String intervals){
 
-        for(int i = 3, l = 2; cell("F"+i,false) != null; i++, l++){
-            cell("G" + i).setCellFormula("COUNTIFS(" + intervals + "," + "\"<=\" & F" + i + ", " + intervals + ", " + "\">=\" & E" + i + ")");
+    public void classes(){
+
+
+//        cell("D8").setCellFormula("LEN(D7)");
+//
+//        evaluator = w.getCreationHelper().createFormulaEvaluator();
+//        valueCell = evaluator.evaluate(cell("D8"));
+//
+//        StringBuilder zeros = new StringBuilder("0");
+//
+//        for(int i = 1; i < valueCell.getNumberValue() - 1; i++){
+//            zeros.append("0");
+//        }
+//
+//        cell("D7").setCellFormula("CEILING(C7, 5" + zeros + ")");
+
+
+        cell("E3").setCellValue(infos.get("minL"));
+        cell("F3").setCellFormula("E3+$D$7");
+
+        evaluator = w.getCreationHelper().createFormulaEvaluator();
+        valueCell = evaluator.evaluate(cell("F3"));
+
+        double x = valueCell.getNumberValue();
+
+        int i = 4;
+
+        while(infos.get("Xmax") > x){
+
+            cell("E" + i).setCellFormula("F" + (i - 1));
+            cell("F" + i).setCellFormula("E" + i + "+$D$7");
+
+            evaluator = w.getCreationHelper().createFormulaEvaluator();
+            valueCell = evaluator.evaluate(cell("F" + i));
+
+            x = valueCell.getNumberValue();
+
+            i++;
+
+        }
+
+    }
+    public void frequency(String intervals) {
+
+        for (int i = 3, l = 2; cell("F" + i, false) != null; i++, l++) {
+            cell("G" + i).setCellFormula("COUNTIFS(" + intervals + "," + "\"<=\" & F" + i + " - 0.1, " + intervals + ", " + "\">=\" & E" + i + ")");
         }
 
         //=CONT.SES(A1:A100001; "<=" & F3; A1:A100001; ">=65")
 
     }
-    public static void setMidPoint() {
+
+    public void setMidPoint() {
 
         for (int i = 3; sheet.getRow(i - 1).getCell(6) != null; i++) {
             cell("H" + i).setCellFormula("(E" + i + " + F" + i + ")/2");
@@ -300,7 +399,7 @@ public class CreateExcel {
 
     }
 
-    public static void relativeFrequency() {
+    public void relativeFrequency() {
 
         for (int i = 3; sheet.getRow(i - 1).getCell(6) != null; i++) {
             cell("I" + i).setCellFormula("G" + i + "/$C$2");
@@ -308,7 +407,7 @@ public class CreateExcel {
 
     }
 
-    public static void percentageFrequency() {
+    public void percentageFrequency() {
 
         for (int i = 3; sheet.getRow(i - 1).getCell(6) != null; i++) {
             cell("J" + i).setCellFormula("(G" + i + "*100)/$C$2");
@@ -316,7 +415,7 @@ public class CreateExcel {
 
     }
 
-    public static void cumulativeBellow(String letterOfCell, String letterOfCumulate) {
+    public void cumulativeBellow(String letterOfCell, String letterOfCumulate) {
 
         if (letterOfCumulate.length() != 1 || letterOfCell.length() != 1) {
             System.out.println("Letter provided incorrect");
@@ -337,7 +436,7 @@ public class CreateExcel {
 
     }
 
-    public static void cumulativeAbove(String letterOfCell, String letterOfCumulate) {
+    public void cumulativeAbove(String letterOfCell, String letterOfCumulate) {
 
         if (letterOfCumulate.length() != 1 || letterOfCell.length() != 1) {
             System.out.println("Letter provided incorrect");
@@ -365,35 +464,34 @@ public class CreateExcel {
 
     }
 
-
     //Função q retorne a celula por uma string => "C5" -> row.get(2).getCell(4)
-    public static Cell cell(String localization, Boolean createCell) {
+    public Cell cell(String localization, Boolean createCell) {
 
         int letter = 0;
         int number = 0;
 
-        if(!createCell){
+        if (!createCell) {
 
-        var fields = localization.replaceAll(String.valueOf(localization.charAt(0)), localization.charAt(0) + ",").split(",");
+            var fields = localization.replaceAll(String.valueOf(localization.charAt(0)), localization.charAt(0) + ",").split(",");
 
-        if (!(fields[0].charAt(0) >= 65 && fields[0].charAt(0) <= 90)) {
-            System.out.println("Letter input incorrect");
-            return sheet.getRow(0).getCell(0);
-        }
+            if (!(fields[0].charAt(0) >= 65 && fields[0].charAt(0) <= 90)) {
+                System.out.println("Letter input incorrect");
+                return sheet.getRow(0).getCell(0);
+            }
 
-        letter = (fields[0].charAt(0)) - 65;
-        number = Integer.parseInt(fields[1]) - 1;
+            letter = (fields[0].charAt(0)) - 65;
+            number = Integer.parseInt(fields[1]) - 1;
 
 
-        }else{
+        } else {
             return cell(localization);
         }
 
-        try{
+        try {
 
-        return sheet.getRow(number).getCell(letter);
+            return sheet.getRow(number).getCell(letter);
 
-        }catch(Exception _){
+        } catch (Exception _) {
 
             return null;
 
